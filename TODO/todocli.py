afcf0e96770975a0319ed3a697ceb77c05272aad
Skyino7 <christambassador12@gmail.com>
2023-11-21 23:22:40 +0000

    updated

--- TODO/todocli.py
+++ TODO/todocli.py
@@ -12,14 +12,36 @@ def add(task: str, category: str):
     typer.echo(f"adding {task}, {category}")
 
 
-@app.command()
+@app.command(short_help='deletes an item')
 def delete(position: int):
     typer.echo(f"deleting {position}")
 
 
-@app.command()
+@app.command(short_help='updates an item')
 def update(postion: int, task: str = None,  category: str = None):
     typer.echo(f"updating {postion}")
+
+
+@app.command()
+def complete(position: int):
+    typer.echo(f"complete {position}")
+
+
+@app.command()
+def show():
+    tasks = [("Task1", "Study"), ("Task2", "Read")]
+    console.print("[bold magenta]Todos[/bold magenta]")
+
+    table = Table(show_header=True, header_style="bold blue")
+    table.add_column("#", style="dim", width=6)
+    table.add_column("Todo", min_width=20)
+    table.add_column("Category", min_width=20, justify="right")
+    table.add_column("Done", min_width=12, justify="right")
+
+    for idx, task in enumerate(tasks, start=1):
+        c = get_category_color(task[1])
+        is_done_str = 'Done' if True == 2 else 'On it'
+        table.add_row(str(idx), task[0], f'[{c}]{task[1]}[/{c}]', is_done_str)
 
 
 if __name__ == "__main__":
