1509ba897a61665a8269bc2514f7ae0e2e8eec79
Skyino7 <christambassador12@gmail.com>
2023-11-22 00:59:36 +0000

    model added

--- TODO/todocli.py
+++ TODO/todocli.py
@@ -10,21 +10,25 @@ app = typer.Typer()
 @app.command(short_help='adds an item')
 def add(task: str, category: str):
     typer.echo(f"adding {task}, {category}")
+    show()
 
 
 @app.command(short_help='deletes an item')
 def delete(position: int):
     typer.echo(f"deleting {position}")
+    show()
 
 
 @app.command(short_help='updates an item')
 def update(postion: int, task: str = None,  category: str = None):
     typer.echo(f"updating {postion}")
+    show()
 
 
 @app.command()
 def complete(position: int):
     typer.echo(f"complete {position}")
+    show()
 
 
 @app.command()
@@ -38,10 +42,18 @@ def show():
     table.add_column("Category", min_width=20, justify="right")
     table.add_column("Done", min_width=12, justify="right")
 
+    def get_category_color(category):
+        COLORS = {'Learn': 'cyan', 'YouTube': 'red',
+                  'Read': 'blue', 'Study': 'green'}
+        if category in COLORS:
+            return COLORS[category]
+        return 'white'
+
     for idx, task in enumerate(tasks, start=1):
         c = get_category_color(task[1])
         is_done_str = 'Done' if True == 2 else 'On it'
         table.add_row(str(idx), task[0], f'[{c}]{task[1]}[/{c}]', is_done_str)
+    console.print(table)
 
 
 if __name__ == "__main__":
